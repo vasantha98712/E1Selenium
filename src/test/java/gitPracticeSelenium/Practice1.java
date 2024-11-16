5310357ec76b7e6be07684dda6fe90c4bfa2e613
sachi <sachi@LAPTOP-FTMD2V76>
2024-11-16 08:46:50 +0530

    Practice class is done

---
+++ src/test/java/gitPracticeSelenium/Practice1.java
@@ -0,0 +1,13 @@
+package gitPracticeSelenium;
+
+import org.testng.annotations.Test;
+
+public class Practice1 
+{
+		@Test
+		public void sample()
+		{
+			System.out.println("This is 2nd commit");
+		}
+
+}
