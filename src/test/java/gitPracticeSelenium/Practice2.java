85e5009c4e3cdb591ada6bce131eb406824b6555
sachi <sachi@LAPTOP-FTMD2V76>
2024-11-16 08:58:40 +0530

    3rd commit

---
+++ src/test/java/gitPracticeSelenium/Practice2.java
@@ -0,0 +1,13 @@
+package gitPracticeSelenium;
+
+import org.testng.annotations.Test;
+
+public class Practice2 
+{
+	@Test
+	public void sample()
+	{
+		System.out.println("This is 3nd commit");
+		System.out.println("commit done");
+	}
+}
